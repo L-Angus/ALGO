246668a406885f3a53d06b0bcbe035b86c9ce952
angus.li <angus.li@linux.com>
2024-10-17 20:02:18 +0800

    LinkedList Completed.

--- include/LinkedList.hpp
+++ include/LinkedList.hpp
@@ -3,74 +3,129 @@
 
 #include <memory>
 
-template<typename T>
-struct LinkedNode{
-    LinkedNode(T val) : value(val), pNextNode(nullptr){}
-    T value;
-    LinkedNode<T>* pNextNode;
+template <typename T> struct LinkedNode {
+  LinkedNode(T val) : value(val), pNextNode(nullptr) {}
+  T value;
+  LinkedNode<T> *pNextNode;
 };
 
-template<typename T>
-class LinkedList{
+template <typename T> class LinkedList {
 public:
-    LinkedList(T val) : pHeadNode(new LinkedNode<T>(val)){}
-    ~LinkedList(){
-        clear();
+  LinkedList(T val) : pHeadNode(new LinkedNode<T>(val)) {}
+  ~LinkedList() { clear(); }
+  void push_back(const T &value) {
+    LinkedNode<T> *newNode = new LinkedNode<T>(value);
+    if (!pHeadNode) {
+      pHeadNode = newNode;
+    } else {
+      LinkedNode<T> *curNode = pHeadNode;
+      while (curNode->pNextNode) {
+        curNode = curNode->pNextNode;
+      }
+      curNode->pNextNode = newNode;
     }
-    void push_back(const T& value){
-        LinkedNode<T>* newNode = new LinkedNode<T>(value);
-        if(!pHeadNode){
-            pHeadNode = newNode;
-        }else{
-            LinkedNode<T>* curNode = pHeadNode;
-            while (curNode->pNextNode){
-                curNode = curNode->pNextNode;
-            }
-            curNode->pNextNode = newNode;
-        }
-    }
-    void push_back(T&& value){
-        LinkedNode<T>* newNode = new LinkedNode<T>(value);
-        if(!pHeadNode){
-            pHeadNode = newNode;
-        }else{
-            LinkedNode<T>* curNode = pHeadNode;
-            while (curNode->pNextNode){
-                curNode = curNode->pNextNode;
-            }
-            curNode->pNextNode = newNode;
-        }
-    }
+    m_position++;
+  }
 
-    void clear(){
-        if(!pHeadNode) {
-            std::cout << "No element. Empty LinkedList!" << std::endl;
-            return;
-        }
-        while (pHeadNode->pNextNode){
-            LinkedNode<T> *tempNode = pHeadNode->pNextNode;
-            delete pHeadNode;
-            pHeadNode = tempNode;
-        }
-        delete pHeadNode;
-        pHeadNode = nullptr;
-        std::cout << "--- Cleanup Over! ---" << std::endl;
+  void insert(size_t position, T value) {
+    if (position > m_position) {
+      throw std::runtime_error("Position out of range.");
     }
+    LinkedNode<T> *newNode = new LinkedNode<T>(value);
+    if (position == 0) {
+      newNode->pNextNode = pHeadNode;
+      pHeadNode = newNode;
+    } else {
+      LinkedNode<T> *curNode = pHeadNode;
+      for (size_t curPosition = 0; curPosition < position - 1; ++curPosition) {
+        curNode = curNode->pNextNode;
+      }
+      newNode->pNextNode = curNode->pNextNode;
+      curNode->pNextNode = newNode;
+    }
+  }
 
-    void print(){
-        if(!pHeadNode) {
-            std::cout << "No element. Empty LinkedList!" << std::endl;
-            return;
-        }
-        LinkedNode<T> *curNode = pHeadNode;
-        while(curNode->pNextNode){
-            std::cout << curNode->value << " ";
-            curNode = curNode->pNextNode;
-        }
-        std::cout << curNode->value << std::endl;
+  void remove(size_t position) {
+    if (position > m_position) {
+      throw std::runtime_error("Position out of range.");
     }
+    LinkedNode<T> *toDelete = nullptr;
+    if (position == 0) {
+      toDelete = pHeadNode;
+      pHeadNode = pHeadNode->pNextNode;
+    } else {
+      LinkedNode<T> *curNode = pHeadNode;
+      size_t prev = position - 1;
+      for (size_t curPosition = 0; curPosition < prev; ++curPosition) {
+        curNode = curNode->pNextNode;
+      }
+      toDelete = curNode->pNextNode;
+      curNode->pNextNode = toDelete->pNextNode;
+      toDelete->pNextNode = nullptr;
+    }
+    delete toDelete;
+    m_position--;
+  }
+
+  LinkedNode<T> *access(size_t position) {
+    if (position > m_position) {
+      throw std::runtime_error("Position out of range.");
+    }
+    if (position == 0) {
+      return pHeadNode;
+    }
+    LinkedNode<T> *curNode = pHeadNode;
+    for (size_t curPos = 0; curPos < position - 1; ++curPos) {
+      curNode = curNode->pNextNode;
+    }
+    return curNode->pNextNode;
+  }
+
+  T find(size_t position) {
+    if (position > m_position) {
+      throw std::runtime_error("Position out of range.");
+    }
+    if (position == 0) {
+      return pHeadNode->value;
+    }
+    LinkedNode<T> *curNode = pHeadNode;
+    for (size_t curPos = 0; curPos < position - 1; ++curPos) {
+      curNode = curNode->pNextNode;
+    }
+    return curNode->pNextNode->value;
+  }
+
+  void clear() {
+    if (!pHeadNode) {
+      std::cout << "No element. Empty LinkedList!" << std::endl;
+      return;
+    }
+    while (pHeadNode->pNextNode) {
+      LinkedNode<T> *tempNode = pHeadNode->pNextNode;
+      delete pHeadNode;
+      pHeadNode = tempNode;
+    }
+    delete pHeadNode;
+    pHeadNode = nullptr;
+    std::cout << "--- Cleanup Over! ---" << std::endl;
+  }
+
+  void print() {
+    if (!pHeadNode) {
+      std::cout << "No element. Empty LinkedList!" << std::endl;
+      return;
+    }
+    LinkedNode<T> *curNode = pHeadNode;
+    while (curNode->pNextNode) {
+      std::cout << curNode->value << " ";
+      curNode = curNode->pNextNode;
+    }
+    std::cout << curNode->value << std::endl;
+  }
+
 private:
-    LinkedNode<T>* pHeadNode = nullptr;
+  LinkedNode<T> *pHeadNode = nullptr;
+  size_t m_position = 0;
 };
 
-#endif //LINKED_LIST_HPP
+#endif // LINKED_LIST_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 
 #include "LinkedList.hpp"
-int main(){
-    std::cout << "-- Hello Algorithm --" << std::endl;
-    LinkedList<int> linkedlist(0);
-    linkedlist.push_back(1);
-    linkedlist.push_back(2);
-    linkedlist.push_back(3);
-    linkedlist.push_back(4);
-    linkedlist.push_back(5);
-    linkedlist.print();
+int main() {
+  std::cout << "-- Hello Algorithm --" << std::endl;
+  LinkedList<int> linkedlist(0);
+  linkedlist.push_back(1);
+  linkedlist.push_back(2);
+  linkedlist.push_back(3);
+  linkedlist.push_back(4);
+  linkedlist.push_back(5);
+  linkedlist.print();
 
-    // linkedlist.clear();
-    // linkedlist.print();
+  linkedlist.insert(3, 10086);
+  linkedlist.print();
 
-    // linkedlist.print();
-    return 0;
+  // linkedlist.remove(3);
+  // linkedlist.print();
+
+  try {
+    auto found = linkedlist.access(4);
+    std::cout << found->value << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << " Not found such index." << '\n';
+  }
+
+  try {
+    auto found = linkedlist.find(3);
+    std::cout << found << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << " Not found such index." << '\n';
+  }
+
+  return 0;
 }
